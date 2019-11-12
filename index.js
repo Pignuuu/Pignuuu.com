52f2fdef222b812118752a7a14e86c8d97ba8ff0
Stian <Pignuuu@gmail.com>
2019-11-12 21:26:57 +0100

    Updated Index.html and style.css. Added Zondicons

--- index.js
+++ index.js
@@ -10,17 +10,3 @@ const daysCoded = startedCodingDate => {
 
 document.querySelector(".dc").innerHTML = daysCoded(startedCodingDate);
 
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) {
-        /* Firefox */
-        elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) {
-        /* Chrome, Safari & Opera */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) {
-        /* IE/Edge */
-        elem.msRequestFullscreen();
-    }
-}
