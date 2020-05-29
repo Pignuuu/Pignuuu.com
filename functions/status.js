7f3fce3f6e0ddefa829dec4ae903b6d8022b38a9
Stian <Pignuuu@gmail.com>
2020-05-29 17:53:17 +0200

    Test

---
+++ functions/status.js
@@ -0,0 +1,10 @@
+exports.handler = function(event, context, callback) {
+    // your server-side functionality
+}
+
+exports.handler = function(event, context, callback) {
+    callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+    });
+}
